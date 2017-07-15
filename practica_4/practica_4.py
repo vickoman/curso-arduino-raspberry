a7a6a2d0d2946a625d2356e0b8427d10d64628cc
Victor Diaz <vicmandlagasca@gmail.com>
2017-07-15 15:43:09 -0500

    Improve with try catch and sleep

--- practica_4/practica_4.py
+++ practica_4/practica_4.py
@@ -11,7 +11,12 @@ temperature = api.get_variable("59581dff7625423eb19351e9")
 tree = ET.parse('temperatura_15_07_2017_15_11_49.xml')
 root = tree.getroot() #obtengo la referencia de la raiz
 for item in root:  #obtengo los hijos desde la raiz
-    fecha = int(time.mktime(datetime.datetime.strptime(item.attrib["tiempo"], '%d-%m-%Y %H:%M:%S').timetuple()))
-    fecha = int(fecha * 1e3)
-    response = temperature.save_value({"value": int(float(item.text)),
+	try:
+		fecha = int(time.mktime(datetime.datetime.strptime(item.attrib["tiempo"], '%d-%m-%Y %H:%M:%S').timetuple()))
+    	fecha = int(fecha * 1e3)
+    	response = temperature.save_value({"value": int(float(item.text)),
                                        "timestamp": fecha})
+    	print("Subiendo...")
+    except:
+    	pass
+    time.sleep(0.5)

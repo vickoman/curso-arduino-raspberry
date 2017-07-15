e8f78911bf163302acfa0aeaa047a048a850883e
Victor Diaz <vicmandlagasca@gmail.com>
2017-07-15 14:28:12 -0500

    Correct API-KEY

--- practica_4/practica_2.py
+++ practica_4/practica_2.py
@@ -3,7 +3,7 @@ from ubidots import ApiClient
 import time
 from random import randint
 
-api = ApiClient("kKWPshWijlTpR1BzDbVK9YALXi48Uf")
+api = ApiClient("d66dfcd757f20e235cf72b6040752ae75eb0aa33")
 temperature = api.get_variable("59581dff7625423eb19351e9")
 humedad = api.get_variable("59581df87625423eb2e5514d")
 

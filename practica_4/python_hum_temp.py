a223f3243d5d8fea934a23519ba315c0ecb31dbb
Victor Diaz <vicmandlagasca@gmail.com>
2017-07-15 14:07:56 -0500

    Configuring My usb

--- practica_4/python_hum_temp.py
+++ practica_4/python_hum_temp.py
@@ -1,6 +1,6 @@
 import serial
 
-arduino = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=3.0)
+arduino = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=3.0)
 
 while True:
 	txt = arduino.readline()

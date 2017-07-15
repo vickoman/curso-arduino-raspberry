7c52a35f326769b499e4237978a7341987c4b4c4
Victor Diaz <vicmandlagasca@gmail.com>
2017-07-15 14:33:33 -0500

    End line

--- practica_4/practica_3.py
+++ practica_4/practica_3.py
@@ -1,13 +1,13 @@
+#!/usr/bin/env python
 import serial
 import MySQLdb
 
 arduino = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=3.0)
 #Mysql connection
-#db = MySQLdb.connect(host=arduino, user="root", passwd="root!", db="data")
+db = MySQLdb.connect(host="localhost", user="root", passwd="root!", db="data")
 
 while True:
 	txt = arduino.readline()
 	print(txt)
-	print """--------------"""
 
 arduino.close()

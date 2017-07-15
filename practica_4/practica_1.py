b0cc920a47f2ea44841f8663a0276e19593397a2
Victor Diaz <vicmandlagasca@gmail.com>
2017-07-15 14:24:56 -0500

    Practice 2 - Sending data to ubidots

--- practica_4/practica_1.py
+++ practica_4/practica_1.py
@@ -1,6 +1,9 @@
 import serial
+import MySQLdb
 
 arduino = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=3.0)
+#Mysql connection
+#db = MySQLdb.connect(host=arduino, user="root", passwd="root!", db="data")
 
 while True:
 	txt = arduino.readline()
---
+++ practica_4/practica_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from ubidots import ApiClient
+import time
+from random import randint
+
+api = ApiClient("kKWPshWijlTpR1BzDbVK9YALXi48Uf")
+temperature = api.get_variable("59581dff7625423eb19351e9")
+humedad = api.get_variable("59581df87625423eb2e5514d
+")
+
+while(1):
+	try:
+		response = temperature.save_value({"value": randint(0,10)})
+		response = humedad.save_value({"value": randint(0,10)})
+	except:
+		pass
+	time.sleep(5)

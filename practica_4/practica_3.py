dad8cefa550841144e243800d13b22ee60f93a72
Victor Diaz <vicmandlagasca@gmail.com>
2017-07-15 14:55:50 -0500

    PRactica 3 Download CSV from ubidots

--- practica_4/practica_3.py
+++ practica_4/practica_3.py
@@ -1,33 +1,21 @@
 #!/usr/bin/env python
-import serial
-import MySQLdb
+from ubidots import ApiClient
+import xml.etree.cElementTree as ET
+import time
+import datetime
 
-arduino = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=3.0)
-#Mysql connection
-db = MySQLdb.connect(host="localhost", user="root", passwd="root", db="base_practica")
 
-line = []
+api = ApiClient("d66dfcd757f20e235cf72b6040752ae75eb0aa33")
+temperature = api.get_variable("59581dff7625423eb19351e9")
+#humedad = api.get_variable("59581df87625423eb2e5514d")
 
-def save_data_sensor(sensor, value):
-	cursor = db.cursor()
-	queryinsert = "INSERT INTO data(sensor, valor)VALUES('%s', '%s')" % (sensor, value)
-	try:
-		cursor.execute(queryinsert)
-		db.commit()
-		cursor.close()
-		print("Se insert el valor")
-	except:
-		db.rollback()
-		print("No se inserto el valor")
+response_t = temperature.get_values()
 
-while True:
-	#txt = arduino.readline()
-	#print(txt)
-	for c in arduino.read():
-		line.append(c)
-		if c == '\n':
-			print("Line: %s", line)
-			line = []
-			break
-
-arduino.close()
+if len(response_t):
+    root = ET.Element("datos_temperatura")
+    for t in response_t:
+        temperatura = ET.SubElement(root, "temperatura")
+        temperatura.text = str(t['value'])
+        temperatura.set('tiempo', datetime.datetime.fromtimestamp(t["timestamp"]/1e3).strftime('%d-%m-%Y %H:%M:%S'))
+    tree = ET.ElementTree(root)
+    tree.write("temperatura_"+ time.strftime("%d_%m_%Y_%H_%M_%S") +".xml")
---
+++ practica_4/practica_4.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import serial
+import MySQLdb
+
+arduino = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=3.0)
+#Mysql connection
+db = MySQLdb.connect(host="localhost", user="root", passwd="root", db="base_practica")
+
+line = []
+
+def save_data_sensor(sensor, value):
+	cursor = db.cursor()
+	queryinsert = "INSERT INTO data(sensor, valor)VALUES('%s', '%s')" % (sensor, value)
+	try:
+		cursor.execute(queryinsert)
+		db.commit()
+		cursor.close()
+		print("Se insert el valor")
+	except:
+		db.rollback()
+		print("No se inserto el valor")
+
+while True:
+	#txt = arduino.readline()
+	#print(txt)
+	for c in arduino.read():
+		line.append(c)
+		if c == '\n':
+			print("Line: %s", line)
+			line = []
+			break
+
+arduino.close()

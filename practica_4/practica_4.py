c62b1efb62ad90f625cc371c693828411a9dff1d
Victor Diaz <vicmandlagasca@gmail.com>
2017-07-15 15:22:10 -0500

    Correct Path for xml

--- practica_4/practica_4.py
+++ practica_4/practica_4.py
@@ -8,7 +8,7 @@ import datetime
 api = ApiClient("d66dfcd757f20e235cf72b6040752ae75eb0aa33")
 temperature = api.get_variable("59581dff7625423eb19351e9")
 
-tree = ET.parse('../temperatura_15_07_2017_15_11_49.xml')
+tree = ET.parse('temperatura_15_07_2017_15_11_49.xml')
 root = tree.getroot() #obtengo la referencia de la raiz
 for item in root:  #obtengo los hijos desde la raiz
     fecha = int(time.mktime(datetime.datetime.strptime(item.attrib["tiempo"], '%d-%m-%Y %H:%M:%S').timetuple()))

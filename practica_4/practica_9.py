8feecca1dcbbd41028a9907d2d57646673acb421
Victor Diaz <vicmandlagasca@gmail.com>
2017-07-22 16:49:40 -0500

    Thinkspeak connection #11

--- practica_4/practica_9.py
+++ practica_4/practica_9.py
@@ -1,44 +1,35 @@
-#!/usr/bin/env python
-# Joshwa
-# This code plots the temperature vs time graph online using thingspeak measured by a thermometer.
-# The Thermometer value is read by BrickPi mounted on RaspberryPi.
-# For more information on BrickPi and Thermometer sensor visit http://www.dexterindustries.com/
-# To get started on basics of pushing data to thingspeak visit : www.australianrobotics.com.au/news/how-to-talk-to-thingspeak-with-python-a-memory-cpu-monitor 
-# For the working of thermometer code alone visit : https://github.com/DexterInd/BrickPi_Python/blob/master/Sensor%20Examples/DI-dTemp%20test.py
-# Connect the Thermometer to the SENSOR PORT_3 of BrickPi
 import httplib, urllib
 import time
 import serial
 from BrickPi import *   #import BrickPi.py file to use BrickPi operations
 import math
 from random import randint
-
-
+arduino = serial.Serial('/dev/ttyACM0',baudrate=9600,timeout=3.0)
 def prueba():
-	global arduino
-	#params = urllib.urlencode({'field1': temp, 'key':'FCPI0AH7789024OW'})		
-	headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
-	conn = httplib.HTTPConnection("192.168.10.50:3000")	
-	try:		
-		txt     = arduino.readline()		
-		if "Temp" in txt:			
-			val     = txt[5:7]		
-			conn.request("POST", "/update", {'field1': val, 'key':'QOPT6UV8MICZ7XZA'}, headers)
-			response = conn.getresponse()
-			print temp
-			print response.status, response.reason
-			data = response.read()
-			conn.close()
-		else:
-			print "No encontro temperatura"
-			print txt
-	except:
-		print "connection failed"
+    global arduino
+    while True:
+        txt = arduino.readline()
+        if "Temp" in txt:
+            temp = txt[5:7]
+            params = urllib.urlencode({'field1': temp, 'key':'FCPI0AH7789024OW'})     # use your API key generated in the thingspeak channels for the value of 'key'
+            # temp is the data you will be seding to the thingspeak channel for plotting the graph. You can add more than one channel and plot more graphs
+            headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+            conn = httplib.HTTPConnection("192.168.10.49:3000")
+            try:
+                conn.request("POST", "/update", params, headers)
+                response = conn.getresponse()
+                print temp
+                print response.status, response.reason
+                data = response.read()
+                conn.close()
+            except:
+                print "connection failed"
+    arduino.close()
 
+
+#sleep for 16 seconds (api limit of 15 secs)
 if __name__ == "__main__":
-	arduino = serial.Serial('/dev/ttyACM0',baudrate=9600,timeout=3.0)
-	while True:
-		#thermometer()
-		prueba()
-		time.sleep(16) 
-		arduino.close()
+        while True:
+                #thermometer()
+                prueba()
+                time.sleep(16) 

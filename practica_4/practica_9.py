4a87d258184663243cf4da2dce97b7df24bbd6b1
Victor Diaz <vicmandlagasca@gmail.com>
2017-07-22 16:25:13 -0500

    Thinkspeak connection #5

--- practica_4/practica_9.py
+++ practica_4/practica_9.py
@@ -8,17 +8,27 @@
 # Connect the Thermometer to the SENSOR PORT_3 of BrickPi
 import httplib, urllib
 import time
+import serial
 from BrickPi import *   #import BrickPi.py file to use BrickPi operations
 import math
 from random import randint
 
 def prueba():
 	temp = randint(0,50)
-	params = urllib.urlencode({'field1': temp, 'key':'QOPT6UV8MICZ7XZA'})		
+	#params = urllib.urlencode({'field1': temp, 'key':'QOPT6UV8MICZ7XZA'})		
 	headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
 	conn = httplib.HTTPConnection("192.168.10.50:3000")
+	arduino = serial.Serial('/dev/ttyACM0',baudrate=9600,timeout=3.0)
 	try:
-		conn.request("POST", "/update", params, headers)
+		sleep(5)
+		txt     = arduino.readline()
+		time.sleep(5)
+
+		if "Temp" in txt:
+			sensor  = 1
+			val     = txt[5:7]
+		if sensor==1:
+			conn.request("POST", "/update", {'field1': val, 'key':'QOPT6UV8MICZ7XZA'}, headers)
 		response = conn.getresponse()
 		print temp
 		print response.status, response.reason
